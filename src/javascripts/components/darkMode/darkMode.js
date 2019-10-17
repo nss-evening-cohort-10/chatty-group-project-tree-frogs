f464caaca5ac01b9d5f53363941f3d20b949a5cb
Jamie <phillipsja97@gmail.com>
2019-10-17 12:24:45 -0500

    navbar finished, darkmode almost

--- src/javascripts/components/darkMode/darkMode.js
+++ src/javascripts/components/darkMode/darkMode.js
@@ -5,7 +5,9 @@ import './darkMode.scss';
 const printDarkCheckbox = () => {
   const domString = `
   <div class="darkMode d-flex justify-content-center">
-  <button class="btn btn-primary" id="activateDark">Activate Dark Mode</button>
+  <input type="checkbox" class="custom-control-input" id="customCheck1">
+  <label class="custom-control-label" for="customCheck1">Activate Dark Mode</label>
+  </div>
   </div>
   `;
   utilities.printToDom('dark-mode-checkbox', domString);
@@ -15,15 +17,25 @@ const checkDarkMode = () => {
   $('body').css('background-color', 'black');
   $('.navbar').css('background-color', 'gray');
   $('.navbar').css('color', 'white');
+  $('.dark-logo').toggle();
+  $('.light-logo').toggle();
+};
+
+const activateLightMode = () => {
+  $('body').css('background-color', 'white');
+  $('.navbar').css('background-color', 'rgb(207, 193, 193)');
+  $('.navbar').css('color', 'white');
   $('.light-logo').toggle();
   $('.dark-logo').toggle();
-  // $('.dark-logo').show();
-  // $('.light-logo').hide();
 };
 
 const activateDarkMode = () => {
-  $('#activateDark').on('click', () => {
-    checkDarkMode();
+  $('input[type=checkbox]').click(() => {
+    if ($('input[type=checkbox]').is(':checked')) {
+      checkDarkMode();
+    } else if ($('input:not(:checked')) {
+      activateLightMode();
+    }
   });
 };
 
