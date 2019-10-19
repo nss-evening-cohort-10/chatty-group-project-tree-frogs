f2f01c804bafe105a20c1a0496e1b69dd5717a56
Denise Baker <denviol@yahoo.com>
2019-10-19 10:03:25 -0500

    updated data array

--- src/javascripts/helpers/data/data.js
+++ src/javascripts/helpers/data/data.js
@@ -1,29 +1,36 @@
 const chattyData = [
   {
+    userId: 'batman',
     id: 'chatty1',
     name: 'chatty1',
     message: 'Hello from chatty1',
     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Batman_%28black_background%29.jpg',
   },
   {
+    userId: 'joker',
     id: 'chatty2',
     name: 'chatty2',
     message: 'Hello from chatty2',
     imageUrl: 'https://img1.looper.com/img/gallery/all-the-star-wars-episode-9-rumors-and-spoilers-that-have-leaked-so-far/intro-1529064429.jpg',
   },
   {
+    userId: 'batman',
     id: 'chatty3',
     name: 'chatty3',
     message: 'Hello from chatty3',
     imageUrl: 'https://secure-media1.hotstarext.com/t_web_hs_2_5x/r1/thumbs/PCTV/15/1770001415/PCTV-1770001415-hcdl.jpg',
   },
   {
+    userId: 'joker',
+    // id: '',
     id: 'chatty4',
     name: 'chatty4',
     message: 'Hello from chatty4',
     imageUrl: 'https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/Avatar3.jpg',
   },
   {
+    userId: 'batman',
+    // id: '',
     id: 'chatty5',
     name: 'chatty5',
     message: 'Hello from chatty5',
