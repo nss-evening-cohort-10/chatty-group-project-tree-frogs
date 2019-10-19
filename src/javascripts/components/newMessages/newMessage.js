3366b35ca8753b581458b179c14c698784bbf039
Jamie <phillipsja97@gmail.com>
2019-10-19 13:50:06 -0500

    almost finished

--- src/javascripts/components/newMessages/newMessage.js
+++ src/javascripts/components/newMessages/newMessage.js
@@ -1,15 +1,28 @@
 import $ from 'jquery';
 import utilities from '../../helpers/utilities';
+import data from '../../helpers/data/data';
 
 const printNewMessage = () => {
   let domString = '';
-  $('#chatInput').keyup((event) => {
-    const newMessage = $('#chatInput').val().toLowerCase();
-    if (event.keyCode === 13) {
-      domString += newMessage;
-    }
-    utilities.printToDom('message-container2', domString);
+  const userData = data.getChattyData();
+  userData.forEach((d) => {
+    $('#chatInput').keyup((event) => {
+      const newMessage = $('#chatInput').val().toLowerCase();
+      if (event.keyCode === 13 && ($('#batman') === `${d.userId}`)) {
+        domString += `<div id="message-container2">
+        <div class='chat right> ${newMessage};
+        </div>
+        </div>        
+        `;
+      } else if (event.keyCode === 13 && ($('#joker') === `${d.userId}`)) {
+        domString += `<div id="message-container">
+        <div class='chat left'> ${newMessage}
+        </div>
+        </div>`;
+      }
+    });
   });
+  utilities.printToDom('message-display', domString);
 };
 
 // const printNewMessage = () => {
