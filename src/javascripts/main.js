2b769e93aeba4b9c1bcfe18e9f2b6460b08eb08a
Yitbarek Gitore <yitbareky@gmail.com>
2019-10-20 15:52:09 -0500

    modified timestamp

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,7 @@ import m from './components/messages/messages';
 import dark from './components/darkMode/darkMode';
 import 'bootstrap';
 import menuItems from './components/dropDownMenuItems/dropDownMenuItems';
-
+import t from './components/timeStamp/timeStamp';
 import '../styles/main.scss';
 
 
@@ -12,6 +12,8 @@ const init = () => {
   m.printMessages();
   menuItems.items();
   m.addMessage();
+  t.leftTimeStamp();
+  t.rightTimeStamp();
 };
 
 init();
