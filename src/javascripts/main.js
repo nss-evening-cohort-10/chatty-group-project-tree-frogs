f1f61a3a336290740c342d82a7e996681a1276a5
Denise Baker <denviol@yahoo.com>
2019-10-21 22:39:24 -0500

    fixed mouse leave function and minor errors

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,6 @@
 import m from './components/messages/messages';
 import dark from './components/darkMode/darkMode';
 import newM from './components/newMessages/newMessage';
-import t from './components/timeStamp/timeStamp';
 import e from './components/emoji/emoji';
 import 'bootstrap';
 import '../styles/main.scss';
